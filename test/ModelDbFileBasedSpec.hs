98a8c8a92b20dcf43093e128b6a5be3d8704919a
Amogh Rathore <amoghdroid09@gmail.com>
2019-08-31 22:26:41 +0900

    Added one more test to MonadDbFileBasedSpec

--- test/ModelDbFileBasedSpec.hs
+++ test/ModelDbFileBasedSpec.hs
@@ -6,14 +6,10 @@ import Test.QuickCheck
 import Test.Hspec
 import ModelDbFileBased
 import Control.Monad.State
-import qualified Data.ByteString.UTF8 as BU
-import qualified Data.Serialize as S
-import qualified Data.ByteString as B
-import Control.Monad.Catch
-import Control.Exception
 import Trx
 import TrxGen
 import MockFileRepo
+import Debug.Trace
 
 spec :: Spec 
 spec = do 
@@ -27,3 +23,9 @@ spec = do
                 in  case (res :: Either String Bool) of 
                         Right True -> True 
                         _          -> False
+
+        it "throws descriptive error when file that doesn't exist is tried to be read" $
+            let res = flip evalState EmptyDir . runModelDbFileBased "dir/model.dat" $ readModel
+            in  case (res :: Either String String) of 
+                    Left "File dir/model.dat does not exist." -> True                     
+                    _                                         -> False
