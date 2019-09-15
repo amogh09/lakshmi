43e4cd73cf3359f6abaa5a5b617946e39f9978ef
Amogh Rathore <amoghdroid09@gmail.com>
2019-09-15 10:29:15 +0900

    Added skeleton command-line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,52 @@
 module Main where
 
 import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Command = 
+    Show ShowCommand
+    deriving (Show)
+
+data ShowCommand = 
+        Balance
+    |   Address Int
+    deriving (Show)
+
+balanceParser :: Parser Command 
+balanceParser = pure (Show Balance)
+
+addressParser :: Parser Command 
+addressParser = Show <$> Address 
+    <$> option auto
+        (
+            long "count"
+       <>   short 'c'
+       <>   value 5
+       <>   help "number of addresses to be displayed (latest ones first)"
+       <>   metavar "<integer>"
+        )
+
+showParser :: Parser Command 
+showParser = hsubparser
+    (
+        command "balance" (info balanceParser (progDesc "Show balance"))
+   <>   command "address" (info addressParser (progDesc "Show addresses"))
+    )
+
+parser :: Parser Command 
+parser = hsubparser
+    (
+        command "show" (info showParser (progDesc "Show info"))
+    )
+
+run :: Command -> IO ()
+run c = print c
 
 main :: IO ()
-main = someFunc
+main = run =<< execParser opts where 
+    opts = info (parser <**> helper)
+        (
+            fullDesc
+       <>   progDesc "Testing this"
+        )
